f37cfbc7b8a604a1f719b71af6d13eed9b58c425
MASUKANE <fktn92@gmail.com>
2019-06-22 08:13:49 +0000

    04-01

--- db/migrate/20190619105236_create_users.rb
+++ db/migrate/20190619105236_create_users.rb
@@ -1,10 +1,5 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
+class AddPasswordDigestToUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      
-      t.timestamps
-    end
+    add_column :users, :password_digest, :string
   end
 end
