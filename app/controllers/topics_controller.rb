72c0ee6ede123b19a76572dda68905e60b26b150
MASUKANE <fktn92@gmail.com>
2019-07-04 08:02:51 +0000

    06_itiran

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,8 @@
 class TopicsController < ApplicationController
-  # def index
-  #   @topics = Topic.all
-  # end
-  
+  def index
+    @topics = Topic.all
+  end
+
   def new
     @topic = Topic.new
   end
